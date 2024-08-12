6c225c44fb870b331f02214632aa21767c25f9fe
linhle120705 <linhle.31231023748@st.ueh.edu.vn>
2024-08-12 23:39:29 +0700

    Add files via upload
    upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //baitap01();
+        baitap02();
+    }
+    static void baitap01()
+    {
+        Console.WriteLine("Nhap so vao day: ");
+        string num_str = Console.ReadLine();
+        int num = Convert.ToInt16(num_str);
+        Console.WriteLine($"So nha cua anh A la: {num}");
+    }
+    static void baitap02()
+    {
+        Console.WriteLine("Nhap vao chu vi hinh tron: ");
+        string cv_string = Console.ReadLine();
+        double cv = Convert.ToDouble(cv_string);
+        const double pi = 3.14;
+        double bk = cv /pi/2;
+        double S = bk * bk * pi;
+        Console.WriteLine("ban kinh cua hinh tron la: {0}", bk);
+        Console.WriteLine("dien tich cua hinh tron la: {0}", S);
+    }
+}
