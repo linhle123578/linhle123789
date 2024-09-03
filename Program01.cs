377a1602eac8229fb48e0292d61c7f5ae23265ba
linhle120705 <linhle.31231023748@st.ueh.edu.vn>
2024-09-03 16:56:29 +0700

    Add files via upload

---
+++ Program01.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] answer = new string[4];
+        for (int i = 0; i < answer.Length; i++)
+        {
+            answer[i] = Console.ReadLine();
+        }
+        for (int i = 0; i < answer.Length; i++)
+        {
+            answer[i] = Console.WriteLine();
+        }
+    }
+}
